2cb6021fd13d0b04a7ed7fb9841b84c9bb630e4e
Holygon <noahseba@gmail.com>
2019-10-16 11:26:18 +0200

    Initial import of files.

---
+++ README.md
@@ -0,0 +1,55 @@
+# Holygon's Python Projects
+
+This is my repository for my projects using Python.
+I taught myself Python some years ago, but life got in the way, and I never really got profecient. This is my attempt at getting back into it.
+I plan on using this repository as a place to store my projects, and to use the README.md file as a tool to write project reports to gain an overview on my progess.
+The report structure should also serve as a method to quickly gain an overview of my thought process behind the creation of the program, to refer back to later.
+
+*For all intents and purposes this repo is made for my private use and viewing, but if you are interested in observing the process of a novice learning the wonderful and scary world of programming, then feel free to read my reports.*
+
+## Report Structure
+
+I want to structure my project reports in a rigid manner to effectivise the writing process, and to be able to have an easy way to look back on the thougt process behind each project.
+
+**Project structure guidelines:**
+
+1. Introduction - What is the purpose of the program? / What does the program do?
+2. Goals - What are my goals for this project? (Learning, Functionality, Uses, etc.)
+3. 
+
+## Projects | Ordered from oldest to newest
+
+### dog.py | Teaching Arthur the Dog to sit by giving him treats
+
+#### Introduction - What is the program
+
+The program **dog.py** is a "game" where you have to teach your dog, 'Arthur the Dog' to sit on command.
+Arthur is a stubborn dog though, and refuses to even try to listen unless you give him the specific amount of treats he wants!
+Arthur is a very smart dog, and for some kind of reason he is capable of understanding the concept of numbers, and due to what either amounts to telepathy or the ability to speak, he can communicate the exact number of treats he wants to be satisfied...
+He will eat anything you give to him though, so be careful not to overfeed him, as he could get sick!
+
+#### Goals - What I set out to achieve with this program
+
+This is the first program I wrote after my hiatus. My goals for this project were:
+
+* Regaining my knowledge of Python 3 syntax
+* Getting into the algorithmic mindset again
+* Creating a program with "game-y" mechanics, so the user is able to interact with it
+* Creating failsafes for wrong user-input (ie. writing letters somewhere numbers are the expected input)
+* Making the code readable
+* Writing extensive comments to individual lines, to have somewhere to look up the methods I used for future projects
+* Giving the program a "story element", so it would be easier to share with friends for contructive critisism and input.
+
+I figured these goals would help me to understand the fundamentals of Python again, and give me a foundation for future projects.
+I also wanted to make something fun, that I could share with people to regain my excitement for programming.
+
+#### 
+
+The concept of the dog training program came from the idea:
+
+> How would I explain what programming is - and how it worked - to a child with no prior knowledge on the subject?
+
+My answer to this was to explain the algorithmic proces of making a dog understand what "sit" meant through the use of classical conditioning (ie. giving treats for obeying the command).
+Now I'm not in the business of making dog simulator games, so it became more of a process of a "how many treats does the dog want, before it will sit?".
+As the project went on, the *explaining to a child* idea became limiting, and I decided to abandon that, so I could delve into the more "advanced" functions Python offers.
+
---
+++ dog.py
@@ -0,0 +1,197 @@
+import time
+
+# - Functions - #
+ 
+def dogSit():
+    
+    # Data    
+    treatsEaten = 0
+    treatsToSit = 50
+
+    # While Command | Will keep running until treatsEaten >= treatsToSit      
+    while treatsEaten < treatsToSit:
+        
+        # To insure the initial message won't keep on displaying
+        if treatsEaten == 0: 
+            print('\nArthur has not eaten enough treats to sit yet.') 
+
+        # User input of how many treats will be given the Arthur the dog
+        # Using "try and catch" method to ensure input will be positive interger
+        while True:
+            try:
+                giveTreatAmount = int(input('How many treats do you want to give to Arthur?: '))
+            except ValueError:      # Ensure integer
+                print("\nYou have to use numbers to say how many treats Arthur will get! Try again.\n")
+                continue
+            
+            if giveTreatAmount < 0: #Ensure positve
+                print("\nYou can't take treats from Arthur! You monster! Please don't use negative numbers.\n")
+                continue
+            else:
+                break # Input was parsed
+        treatsEaten += giveTreatAmount
+    
+        # If you give Arthur too many treats (>100) the game will print a Game over screen
+        # Using "raise SystemExit" the program will end after the Game over screen
+        if treatsEaten > 100:
+            print("\nOh no! You have given too many treats to Arthur, and now he is sick!")
+            print("Now he will never learn to sit :-( ")
+            gameOver =  r''' # Using 'r' in front of string, to turn into a raw-string. To prevent backslash errors
+
+            G A M E  O V E R
+
+              /^-----^\\
+              V  o o  V
+               |  Y  |
+                \ Q /
+                / - \\
+                |    \\
+                |     \     )
+                || (___\====
+
+                        '''
+            print(gameOver)
+            time.sleep(0.8)
+            replayLose()
+        
+        # Using the break function the program will go to the Victory Condition if treatsEaten >= treatsToSit
+        # This is done to skip the treatData_1 message, as not to show two similar messages
+        elif (treatsEaten >= treatsToSit):
+            break
+
+        # Using .format to insert variables into middle of string
+        treatData_1 = "\nArthur has now eaten {} treats, and he needs {} treats to learn to sit\n"
+        print(treatData_1.format(treatsEaten,treatsToSit - treatsEaten))
+    
+
+    # Victory Condition | Arthur the dog has gotten enough treats to finally want to learn to sit!
+    treatData_2 = "\nArthur has now eaten {} treats, and has learned to sit! Congratulations!"
+    print(treatData_2.format(treatsEaten))
+    dogPic = r'''
+
+             V I C T O R Y !
+
+            |\_/|                  
+            | @ @   Woof! 
+            |   <>              _  
+            |  _/\------____ ((| |))
+            |               `--' |   
+        ____|_       ___|   |___.' 
+        /_/_____/____/_______|
+              
+            '''
+    
+    print(dogPic)
+    time.sleep(0.8)
+
+def replayWin():
+    
+    # Message
+    print("\n\nCongratulations, you have won the game! Do you wan't to play again?")
+    
+    # Y/N Input
+    while True:
+        replayInput = str(input("Type 'Y' for Yes | Type 'N' for No (Y/N): ")).upper()
+        if replayInput not in ('Y','N'):
+            print("Please choose a valid choice\n")
+        else:
+            break
+
+    if replayInput == "Y":
+        print("\nRestarting game")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        startProgram()
+    
+    elif replayInput == "N":
+        print("Quitting game")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.2)
+        print("\nGoodbye")
+        time.sleep(0.6)
+        raise SystemExit
+    
+    else:
+        print("Error. This should not ever happen. Good job")
+        time.sleep(0.5)
+        raise SystemExit    
+
+def replayLose():
+    
+    # Message
+    print("\nOh no, you lost the game! Do you wan't to try again?")
+    
+    # Y/N Input
+    while True:
+        replayInput_2 = str(input("Type 'Y' for Yes | Type 'N' for No (Y/N): ")).upper()
+        if replayInput_2 not in ('Y','N'):
+            print("Please choose a valid choice\n")
+        else:
+            break
+
+    if replayInput_2 == "Y":
+        print("\nRestarting game")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        startProgram()
+    
+    elif replayInput_2 == "N":
+        print("Quitting game")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.8)
+        print(".")
+        time.sleep(0.2)
+        print("\nGoodbye")
+        time.sleep(0.6)
+        raise SystemExit
+    
+    else:
+        print("Error. This should not ever happen. Good job")
+        time.sleep(0.5)
+        raise SystemExit    
+
+def introStory():
+    story = r'''
+
+                    A R T H U R  T H E  D O G
+
+                          / \__
+                         (    @\___
+                         /         O
+                        /   (_____/  < HELP ME LEARN TO SIT
+                       /_____/   U
+
+   Arthur is your new dog, and you want to teach him to sit on command.
+      He is a stubborn dog, and refuses to follow your instructions.
+        Give him the treats he needs to finally listen to you!
+              Be careful not to overfeed him though.
+
+    '''
+    print(story)
+
+def startProgram():
+    introStory()
+    dogSit()
+    replayWin()
+
+# - Execution of Program - #
+
+startProgram()
